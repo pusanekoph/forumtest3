85236226071e402c3733aa7d1285c9377b49c710
Pusaneko • ᜉᜒᜎᜒᜉᜒᜈᜐ᜔ <90695286+pusanekoph@users.noreply.github.com>
2022-04-28 17:14:10 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,67 @@
+var defaultThreads = [
+    {
+        id: 1,
+        title: "Thread 1",
+        author: "Admin",
+        date: Date.now(),
+        content: "Thread content",
+        comments: [
+            {
+                author: "Jack",
+                date: Date.now(),
+                content: "Hey there"
+            },
+            {
+                author: "Arthur",
+                date: Date.now(),
+                content: "Hey to you too"
+            }
+        ]
+    },
+    {
+        id: 2,
+        title: "Thread 2",
+        author: "Admin",
+        date: Date.now(),
+        content: "Thread content 2",
+        comments: [
+            {
+                author: "Jack",
+                date: Date.now(),
+                content: "Hey there"
+            },
+            {
+                author: "Arthur",
+                date: Date.now(),
+                content: "Hey to you too"
+            }
+        ]
+    },
+    {
+        id: 3,
+        title: "Thread 3",
+        author: "Admin",
+        date: Date.now(),
+        content: "Thread content 3",
+        comments: [
+            {
+                author: "Jack",
+                date: Date.now(),
+                content: "Hey there"
+            },
+            {
+                author: "Arthur",
+                date: Date.now(),
+                content: "Hey to you too"
+            }
+        ]
+    }
+]
+
+var threads = defaultThreads
+if (localStorage && localStorage.getItem('threads')) {
+    threads = JSON.parse(localStorage.getItem('threads'));
+} else {
+    threads = defaultThreads;
+    localStorage.setItem('threads', JSON.stringify(defaultThreads));
+}
